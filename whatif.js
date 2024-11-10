0ac32126511e5a8e1b7cb5b88ba049cd1852b733
coder1110111 <ayush.yadav.dev610@gmail.com>
2024-11-10 15:18:29 +0530

    Adding a multiplication function
    Adding a new multiplication funmction

--- whatif.js
+++ whatif.js
@@ -1,8 +1,13 @@
 function addnumbers(num1,num2){
   return num1 + num2;
 }
+function multiplication(num1,num2){
+  return num1*num2;
+}
 
 let a=5;
 let b=10;
 let c=addnumbers(a,b);
-console.log(c);
+let d=multiplication(a,b);
+console.log(`Addition : ${c}`);
+console.log(`Multiplication : ${d}`);
